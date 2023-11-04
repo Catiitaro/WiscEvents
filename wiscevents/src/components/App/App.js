2c9b881f0211b5c3de50044b0442a8fb1a6eb057
nautomo <149899507+nautomo23@users.noreply.github.com>
2023-11-04 17:12:33 -0500

    worked on navbar

---
+++ wiscevents/src/components/App/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function MyButton() {
+  return <button class="button"> I'm a button </button>;
+}
+
+function App() {
+  return (
+    <div class="header">
+      <a href="#default" class="logo">CompanyLogo</a> 
+      <button class="button"> Explore Events </button>
+      <button class="button"> Button 2 </button>
+      <button class="button"> Button 3 </button>
+      <button class="button"> Button 4 </button>
+      
+    </div>
+  );
+}
+
+export default App;
