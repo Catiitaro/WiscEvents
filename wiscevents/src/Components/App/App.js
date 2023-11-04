ba7c00694c0b39abcd57760c2d8ce08473a6898a
Colin Luangrath <colinluangrath@gmail.com>
2023-11-04 17:46:21 -0500

    push for group request

--- wiscevents/src/Components/App/App.js
+++ wiscevents/src/Components/App/App.js
@@ -1,9 +1,34 @@
+import { useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "890082271248-ork4qes8cfc8a9beak1eks9luru2mvc9.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large"}
+    )
+
+
+  }, []);
+  
+
   return (
     <div className="App">
-      Hello World
+      <div id="signInDiv">
+
+      </div>
     </div>
   );
 }
