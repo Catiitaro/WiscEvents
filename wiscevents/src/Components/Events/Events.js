f5dc83548ab4ca9fbf64d64077604519bb21b7d6
Colin Luangrath <colinluangrath@gmail.com>
2023-11-05 09:58:37 -0600

    boop

--- wiscevents/src/Components/Events/Events.js
+++ wiscevents/src/Components/Events/Events.js
@@ -1,14 +1,22 @@
-import {
-    useParams
-  } from "react-router-dom";
-  import './Events.css'
+import { useParams } from "react-router-dom";
+import './Events.css';
+
 export default function Events(props) {
-    let {eventId} = useParams();
-    let event = props.events[eventId-1]
-    return (
+  let { eventId } = useParams();
+  let event = props.events[eventId - 1];
+
+  return (
     <div className="events">
+      <div className="event-image-container">
+        <img className="event-image" src={event.image} alt={event.title} />
+      </div>
+      <div className="event-details">
         <h1 className="event-title">{event.title}</h1>
-        <img className="event-image" src={event.image}/>
-        <p className="event-description" dangerouslySetInnerHTML={{__html: event.description}}></p>
-    </div>)
+        <p className="event-description" dangerouslySetInnerHTML={{ __html: event.description }}></p>
+        <p className="event-location">{"Location: " + event.location}</p>
+        <p className="event-date">{"Date: " + event.date}</p>
+        <p className="event-time">{"Time: " + event.time}</p>
+      </div>
+    </div>
+  );
 }
--- wiscevents/src/Components/Home/Home.js
+++ wiscevents/src/Components/Home/Home.js
@@ -1,4 +1,3 @@
-// Home.js
 import React from 'react';
 import person1Image from '../../images/person1.jpeg';
 import person2Image from '../../images/person2.jpeg';
@@ -9,26 +8,28 @@ function Home() {
     <div className="home">
       <header className="header">
         <h1>Welcome to Your Next Event Website</h1>
-        <p>Discover and create events in your area.</p>
       </header>
       <section className="features">
         <div className="feature">
           <h2>Explore Events</h2>
-          <p>Explore a diverse range of events right in your local area, from cultural festivals to sports tournaments, and from tech meetups to art exhibitions.
-Discover a multitude of opportunities to engage with your community and fellow enthusiasts, whether you're interested in networking, entertainment, learning, or simply having fun.
-Stay informed about the latest happenings in your vicinity, ensuring you never miss out on exciting local experiences and gatherings.
-With our platform, you have access to a rich selection of events that cater to different tastes and interests, making it easier than ever to find the perfect event to attend.</p>
+          <ul className="feature-list">
+            <li>Discover a diverse range of local events</li>
+            <li>From cultural festivals to sports tournaments</li>
+            <li>Tech meetups to art exhibitions</li>
+            <li>Opportunities to engage with your community</li>
+            <li>Networking, entertainment, learning, and fun</li>
+          </ul>
         </div>
         <div className="feature">
           <h2>Create Events</h2>
           <p>Become an event organizer and bring your unique ideas to life by hosting events that reflect your passions and goals.
-Showcase your creativity and expertise by curating events that resonate with your community, whether it's a charity run, an art workshop, or a tech conference.
-Share your events with a wide audience, attracting attendees who share your enthusiasm and want to participate in the experiences you create.
-Our platform provides you with the tools and support you need to plan and promote your events, ensuring that your initiatives receive the attention they deserve.
-By hosting events, you contribute to building a vibrant and interconnected community, where people can come together to learn, collaborate, and enjoy shared interests.</p>
+            Showcase your creativity and expertise by curating events that resonate with your community, whether it's a charity run, an art workshop, or a tech conference.
+            Share your events with a wide audience, attracting attendees who share your enthusiasm and want to participate in the experiences you create.
+            Our platform provides you with the tools and support you need to plan and promote your events, ensuring that your initiatives receive the attention they deserve.
+            By hosting events, you contribute to building a vibrant and interconnected community, where people can come together to learn, collaborate, and enjoy shared interests.</p>
         </div>
-        <h2>What People Are Saying</h2>
       </section>
+      <h2>What People Are Saying</h2>
       <section className="testimonials">
         <div className="testimonial">
           <img src={person1Image} alt="Person 1" />
