8164ef1cbeb7df0fa8a9cb0f1082a56ac2c1db3c
MaisyDoge <45347378+LCodingX@users.noreply.github.com>
2023-11-04 19:24:50 -0500

    test commit to nataliebranch2 with logo

--- wiscevents/src/Components/Navbar/Navbar.js
+++ wiscevents/src/Components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
 import './Navbar.css'
 import Button from '../Button/Button'
+import logo from '../../images/wiscevents1.webp'
 export default function Navbar() {
     return (<div class="header">
-    <a href="#default" class="logo">CompanyLogo</a>
+    <img src={logo} class="logo"/>
     <Button name="Explore Events"/>
     <Button name="Create New Event"/>
     <Button name="Profile"/>
