e72573af72041853c80280f4e877910441ea526f
nautomo <149899507+nautomo23@users.noreply.github.com>
2023-11-04 20:22:04 -0500

    profile pic on right, resizing

--- wiscevents/src/Components/Navbar/Navbar.js
+++ wiscevents/src/Components/Navbar/Navbar.js
@@ -1,10 +1,16 @@
 import './Navbar.css'
 import Button from '../Button/Button'
+import logo from '../../images/wiscevents1.webp'
+
 export default function Navbar() {
-    return (<div class="header">
-    <a href="#default" class="logo">CompanyLogo</a>
+    return (
+    <div class="header">
+    <a href="https://www.w3schools.com"> <img src={logo} class="logo"/> </a>
     <Button name="Explore Events"/>
     <Button name="Create New Event"/>
-    <Button name="Profile"/>
-    </div>);
+    <div class="header-right">
+        <Button name="Profile"/>
+    </div>
+    </div>
+    );
 }
