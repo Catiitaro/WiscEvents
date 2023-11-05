c2d955fec2352b281c91c45c7c73fdb497adf27a
MaisyDoge <45347378+LCodingX@users.noreply.github.com>
2023-11-05 09:24:18 -0600

    small fix

--- wiscevents/src/Components/Events/Events.js
+++ wiscevents/src/Components/Events/Events.js
@@ -8,6 +8,7 @@ export default function Events(props) {
     return (
     <div className="events">
         <h1 className="event-title">{event.title}</h1>
+        <img className="event-image" src={event.image}/>
         <p className="event-description" dangerouslySetInnerHTML={{__html: event.description}}></p>
     </div>)
 }
