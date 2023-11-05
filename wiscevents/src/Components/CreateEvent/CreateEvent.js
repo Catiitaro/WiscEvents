808e4289c94dd19b7f2ee05bfb499a7fa01ba555
Colin Luangrath <colinluangrath@gmail.com>
2023-11-05 02:50:21 -0600

    implemented the create new event page

--- wiscevents/src/Components/CreateEvent/CreateEvent.js
+++ wiscevents/src/Components/CreateEvent/CreateEvent.js
@@ -1,84 +1,127 @@
-// React Component (EventForm.js)
-
 import React, { useState } from 'react';
+import './CreateEvent.css'; // Create a CSS file for your component
+import blankimage from '../../images/blank-image.png'
 
-function EventForm() {
-  const [events, setEvents] = useState([]);
-  const [image, setImage] = useState('');
+function CreateEvent() {
+  const [image, setImage] = useState(null);
   const [location, setLocation] = useState('');
   const [cost, setCost] = useState('');
   const [description, setDescription] = useState('');
   const [user, setUser] = useState('');
-  const [id, setId] = useState('');
+  const [events, setEvents] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    const imageUrl = URL.createObjectURL(file);
+    setImage(imageUrl);
+  };
+
+  const clearInputFields = () => {
+    setImage(null);
+    setLocation('');
+    setCost('');
+    setDescription('');
+    setUser('');
+  };
 
   const createEvent = () => {
-    const newEvent = {
+    const id = events.length + 1;
+    const event = {
       image,
       location,
       cost,
       description,
       user,
-      id,
+      id: id.toString(),
     };
 
-    setEvents([...events, newEvent]);
-
-    // Clear input fields after creating the event
-    setImage('');
-    setLocation('');
-    setCost('');
-    setDescription('');
-    setUser('');
-    setId('');
+    setEvents([...events, event]);
+    clearInputFields(); // Clear input fields and image after creating the event
   };
 
   const displayEvent = (event) => {
-    console.log('Event Image:', event.image);
-    console.log('Location:', event.location);
-    console.log('Cost:', event.cost);
-    console.log('Description:', event.description);
-    console.log('User:', event.user);
-    console.log('Event ID:', event.id);
-    console.log('\n');
+    setSelectedEvent(event);
   };
 
   return (
-    <div>
-      <h1>Create Event</h1>
-      <div>
-        <label>Image:</label>
-        <input type="text" value={image} onChange={(e) => setImage(e.target.value)} />
+    <div className="create-event">
+      <div className="left-column">
+        <div className="image-and-description">
+        <h2>Event Details</h2>
+          <label htmlFor="imageUpload" className="custom-file-upload">
+            <img
+              src={image || blankimage}
+              alt="Event Image"
+              className="event-image"
+              id="imagePreview"
+            />
+            <input
+              type="file"
+              id="imageUpload"
+              accept="image/*"
+              onChange={handleImageUpload}
+              style={{ display: 'none' }}
+            />
+          </label>
+          <label>Description:</label>
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="auto-expand-textarea"
+          />
+        </div>
       </div>
-      <div>
-        <label>Location:</label>
-        <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
-      </div>
-      <div>
-        <label>Cost:</label>
-        <input type="text" value={cost} onChange={(e) => setCost(e.target.value)} />
-      </div>
-      <div>
-        <label>Description:</label>
-        <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
-      </div>
-      <div>
-        <label>User:</label>
-        <input type="text" value={user} onChange={(e) => setUser(e.target.value)} />
-      </div>
-      <div>
-        <label>ID:</label>
-        <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
-      </div>
-      <button onClick={createEvent}>Create Event</button>
+      <div className="event-input">
+  <input
+    type="text"
+    value={location}
+    onChange={(e) => setLocation(e.target.value)}
+    placeholder="Location"
+  />
+</div>
+<div className="event-input">
+  <input
+    type="text"
+    value={cost}
+    onChange={(e) => setCost(e.target.value)}
+    placeholder="Cost"
+  />
+</div>
+<div className="event-input">
+  <input
+    type="text"
+    value={user}
+    onChange={(e) => setUser(e.target.value)}
+    placeholder="User"
+  />
+</div>
 
-      <h2>Event Details</h2>
+      <button onClick={createEvent} className="create-button">
+        Create Event
+      </button>
+
       {events.map((event, index) => (
-        <div key={index}>
+        <div key={index} className="event-item">
           <button onClick={() => displayEvent(event)}>Display Event Details</button>
         </div>
       ))}
+
+      {selectedEvent && (
+        <div className="selected-event">
+          <h2>Selected Event Details</h2>
+          <p>Event Image:</p>
+<img src={selectedEvent.image} alt="Event Image" className="event-image" />
+
+          <p>Location: {selectedEvent.location}</p>
+          <p>Cost: {selectedEvent.cost}</p>
+          <p>Description: {selectedEvent.description}</p>
+          <p>User: {selectedEvent.user}</p>
+          <p>Event ID: {selectedEvent.id}</p>
+        </div>
+      )}
     </div>
   );
 }
 
-export default EventForm;
+export default CreateEvent;
